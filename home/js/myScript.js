e352e81291655ef24578f56f1b8a618aea183813
Sondrick Frondozo <119903912+SnFrondozo@users.noreply.github.com>
2024-12-19 11:24:48 +0800

    adad

--- home/js/myScript.js
+++ home/js/myScript.js
@@ -1,12 +1,34 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed."; 
+
+function updateDemoText(newText) {
+    document.getElementById("demo").innerHTML = newText;
 }
 
-function myFunction2() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
 
-const element = document.getElementsByTagName("p");
-
-document.getElementById("demo").innerHTML = '<br>The text in first paragraph (index 0) is: <br>' + element[0].innerHTML;
-
+function showFirstParagraphText() {
+    const paragraphs = document.getElementsByTagName("p");
+    if (paragraphs.length > 0) {
+        const firstParagraphText = paragraphs[0].innerHTML;
+        updateDemoText(`The text in the first paragraph (index 0) is: ${firstParagraphText}`);
+    } else {
+        updateDemoText("No paragraphs found.");
+    }
 }
+
+
+function changeParagraphText() {
+    updateDemoText("Paragraph changed.");
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const button1 = document.querySelector('button[onclick="myFunction1()"]');
+    const button2 = document.querySelector('button[onclick="myFunction2()"]');
+    
+    if (button1) {
+        button1.addEventListener("click", () => changeParagraphText());
+    }
+
+    if (button2) {
+        button2.addEventListener("click", () => showFirstParagraphText());
+    }
+});
